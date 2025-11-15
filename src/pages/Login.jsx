5a489679e5e75f1af99460cc43a1296d5f764887
Ewa <ewahjelm84@gmail.com>
2025-11-16 00:42:12 +0100

    Login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+import LoginForm from "../components/LoginForm";
+
+export default function Login () {
+    const { login } = useAuth();
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    async function handleLogin({ username, password }) {
+        setError(null);
+    try {
+        await login(username, password);
+        navigate("/chat")
+    } catch (err) {
+    setError(err.message || "Ett fel uppstod vid inloggning");
+    }
+}
+
+return (
+<div className="login-container">
+    <h4>Välkommen till chat-appen</h4>
+    <h1>Tjatter</h1>
+    <h4>Logga in</h4>
+    <LoginForm onSubmit={handleLogin} />
+    <p>Har du inget konto?  <Link to="/register">Registrera dig här</Link> </p>
+
+    {error && <p style={{ color: "red" }}>{error}</p>}
+</div>
+);
+};
