8ec8fb968a039fab1c8c16f2a6c5ff135e231cdd
Ewa <ewahjelm84@gmail.com>
2025-11-15 21:49:20 +0100

    fortsatt på api anrop

--- src/api/api.js
+++ src/api/api.js
@@ -1,27 +1,71 @@
-// Hämtar CSRF från chatify API
+///////// Anrop mot chatify API
+const API_URL = "https://chatify-api.up.railway.app"
+
+// Hämta CSRF
 export async function fetchCsrfToken() {
-  try {
-    const res = await fetch("https://chatify-api.up.railway.app/csrf", {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    });
+    try {
+        const res = await fetch(`${API_URL}/csrf}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        }
+        });
 
-    if (!res.ok) {
-      throw new Error("Något gick fel när CSRF-token hämtades.");
+        if (!res.ok) {
+        throw new Error("Något gick fel när CSRF-token hämtades.");
+        }
+
+        const data = await res.json();
+        const csrfToken = data.csrfToken;
+
+        if (csrfToken) {
+        sessionStorage.setItem("csrfToken", csrfToken);
+        console.log("CSRF-token sparad:", csrfToken);
+        } else {
+        console.error("Ingen CSRF-token i svaret.");
+        }
+    } catch (err) {
+        console.error(err);
     }
+}
 
-    const data = await res.json();
-    const csrfToken = data.csrfToken;
+//Använd csrf för att logga in med username & password  - få en jwt
+export async function login(username, password) {
+    try {
+        const csrfToken = sessionStorage.getItem("csrfToken");
 
-    if (csrfToken) {
-      sessionStorage.setItem("csrfToken", csrfToken);
-      console.log("CSRF-token sparad:", csrfToken);
-    } else {
-      console.error("Ingen CSRF-token i svaret.");
+        if (!csrfToken) {
+        throw new Error("Ingen CSRF-token finns. Kör fetchCsrfToken() först.");
+        }
+
+        const res = await fetch(`${API_URL}/auth/token`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username,
+            password,
+            csrfToken
+        })
+        });
+
+        if (!res.ok) {
+        throw new Error("Inloggning misslyckades.");
+        }
+
+        const data = await res.json();
+        const token = data.token;
+
+        if (token) {
+        sessionStorage.setItem("jwtToken", token);
+        console.log("JWT sparad:", token);
+        } else {
+        console.error("Ingen token i svaret.");
+        }
+
+        return token;
+    } catch (err) {
+        console.error(err);
     }
-  } catch (err) {
-    console.error(err);
-  }
 }
