7407743f798a3c858877ba2224fc9ab03bdb1b22
Ewa <ewahjelm84@gmail.com>
2025-11-16 00:43:12 +0100

    App with routes and Provider showing / Login start page

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Login, Register, Chat } from './pages'
 import LoginForm from './components/LoginForm'
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
   return (
-    
-    <div className="form-container">
-    <LoginForm />
-    </div>
-/*       <Router>
+    <AuthProvider>
+      <Router>
         <Routes>
           <Route path='/' element={ <Login/> }/>
-          <Route path='/login' element={ <Login/> }/>
+          <Route path='/login' element={ <LoginForm/> }/>
           <Route path='/register' element={ <Register/> }/>
           <Route path='/chat' element={ <Chat/> }/>
         </Routes>
-      </Router> */
+      </Router>
+    </AuthProvider>  
   )
 }
 
