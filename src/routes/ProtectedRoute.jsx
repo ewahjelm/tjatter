4ae10e283544fa38da6c166e5cd476313c19ef2a
Ewa <ewahjelm84@gmail.com>
2025-11-18 12:22:59 +0100

    improved syntax in Protected Route

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -3,12 +3,8 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
     const { user } = useAuth();
-    
-    if (!user) {
-        return <Navigate to="/login" replace />;
-    }
 
-    return children;
+    return user ? children : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
