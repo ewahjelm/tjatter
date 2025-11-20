8a994ea5b6e095fb1c69098ee064c3032fdf600d
Ewa <ewahjelm84@gmail.com>
2025-11-20 14:29:27 +0100

    css

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function setVh() {
+  document.documentElement.style.setProperty('--vh', window.innerHeight * 0.01 + 'px');
+}
+setVh();
+window.addEventListener('resize', setVh);
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
